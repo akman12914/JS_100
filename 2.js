44619ba247a915b5fe7028420f3fceef385c9e65
akman12914 <rnlduadns98@naver.com>
2024-08-21 14:11:31 +0900

    문제2 : 배열의 내장함수

---
+++ 2.js
@@ -0,0 +1,13 @@
+// 데이터
+// var arr = [200, 100, 300];
+// //pass
+// console.log(arr);
+
+// 출력
+// [200, 100, 10000, 300]
+
+
+var arr = [200, 100, 300];
+arr.splice(1,0,10000);
+console.log(arr);
+
