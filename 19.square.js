11c040c2c891d3ce5ddef9fca87c1c64344fb0dc
akman12914 <rnlduadns98@naver.com>
2024-11-01 18:06:04 +0900

    문제19 : 제곱을 구하자

---
+++ 19.square.js
@@ -0,0 +1,14 @@
+let [a, b] = prompt("두 숫자를 입력하시오(공백으로 구분됨)").split(" ");
+
+let square = 1;
+
+for (i = 0; i < b; i++) {
+  square *= a;
+}
+
+console.log(square);
+
+console.log(a);
+
+let = count = 10;
+console.log()
