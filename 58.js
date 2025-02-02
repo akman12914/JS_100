eec315969b705af03fcd33a575a75ca331c7fb67
akman12914 <rnlduadns98@naver.com>
2025-02-02 22:47:38 +0900

    문제58: 콤마 찍기

--- 58.js
+++ 58.js
@@ -1,17 +1,15 @@
 // # 문제58 : 콤마 찍기
 
-// 원범이는 편의점 아르바이트가 끝난 후 정산을 하고자 합니다.
-// 정산을 빨리하고 집에 가고 싶은 원범이는 프로그램을 만들려고 합니다.
-
-// **숫자를 입력받고 천 단위로 콤마(,)를 찍어주세요.**
-
-// 예를 들어, **123456789**를 입력받았으면 **123,456,789**를 출력해야 합니다.
-
 let input = 123456789;
 
 let inputString = Array.from(String(input));
+let result = [];
 
-for (i = 1; i < inputString.length; i++) {
-  if ((i + 1) % 3 == 0) {
+for (i = 0; i < inputString.length; i++) {
+  result.push(inputString[i]);
+
+  if ((i + 1) % 3 == 0 && !(inputString[i + 1] === undefined)) {
+    result.push(",");
   }
 }
+console.log(result.join(""));
