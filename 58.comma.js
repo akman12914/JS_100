3dc7557e2bef4ad763246632f53bb5e2d9ef58ce
akman12914 <rnlduadns98@naver.com>
2025-02-02 22:48:20 +0900

    58번 문제 파일 수정

---
+++ 58.comma.js
@@ -0,0 +1,17 @@
+/* -------------------------------------------------------------------------- */
+/*                              // # 문제58 : 콤마 찍기                             */
+/* -------------------------------------------------------------------------- */
+
+let input = 123456789;
+
+let inputString = Array.from(String(input));
+let result = [];
+
+for (i = 0; i < inputString.length; i++) {
+  result.push(inputString[i]);
+
+  if ((i + 1) % 3 == 0 && !(inputString[i + 1] === undefined)) {
+    result.push(",");
+  }
+}
+console.log(result.join(""));
